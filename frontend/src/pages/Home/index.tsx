af05d9f2b18c9d08031a0583f64bb7332eb9be1e
cliomaas <cliomaas@gmail.com>
2021-11-07 17:47:38 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,29 @@
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link to="/dashboard">
+            <button type="button" className="btn btn-primary">
+              Dashboard
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
 
